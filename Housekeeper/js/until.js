ebbbb7fc39893bf659d60ad9ab666bcd6ec69800
klzhong69 <478958687@qq.com>
2018-05-21 16:39:56 +0800

    1111

---
+++ Housekeeper/js/until.js
@@ -0,0 +1,15 @@
+var dataList = [{
+	"bankNumber": "123456",
+	"bankName": "交通银行",
+	"bankCount": "18000",
+	"bankStartDate": [0, 2],
+	"bankEndDate": [0, 27],
+	"differ": 48
+}, {
+	"bankNumber": "58585",
+	"bankName": "民生银行",
+	"bankCount": "50000",
+	"bankStartDate": [0, 15],
+	"bankEndDate": [1, 4],
+	"differ": 51
+}];
